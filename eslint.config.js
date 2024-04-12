4466684c6278e0971bf6be4ee8cb0c25b2a43052
Mirko Pedico <mirko.pedico@integratedsolutions.it>
2024-04-12 09:46:41 +0200

    first commit

---
+++ eslint.config.js
@@ -0,0 +1,24 @@
+import pluginVue from 'eslint-plugin-vue'
+import eslintConfigPrettier from "eslint-config-prettier";
+
+export default [
+  // add more generic rulesets here, such as:
+  // js.configs.recommended,
+  ...pluginVue.configs['flat/recommended'],
+  // ...pluginVue.configs['flat/vue2-recommended'], // Use this if you are using Vue.js 2.x.
+  eslintConfigPrettier,
+  {
+    rules: {
+      // override/add rules settings here, such as:
+      // 'vue/no-unused-vars': 'error'
+      "vue/max-attributes-per-line": ["error", {
+        "singleline": {
+          "max": 1
+        },      
+        "multiline": {
+          "max": 1
+        }
+      }]
+    }
+  },
+]
---
+++ src/components/TestLint.vue
@@ -0,0 +1,64 @@
+<template>
+  
+  <!-- Esempio vue/max-attributes-per-line (ad es. impostato a 1) -->
+  
+  <!-- ✓ GOOD -->
+  <MyComponent
+    lorem="1"
+    ipsum="2"
+  />
+  <MyComponent
+    lorem="1"
+    ipsum="2"
+    dolor="3"
+  />
+
+  <!-- ✗ BAD -->
+  <MyComponent lorem="1" ipsum="2" />
+  <MyComponent lorem="1" ipsum="2" dolor="3" />
+
+
+
+  <!-- vue/require-v-for-key -->
+
+  <!-- ✓ GOOD -->
+  <ol 
+    v-for="todo in todos" 
+    :key="todo.id">
+  </ol>
+  <!-- ✗ BAD -->
+  <div v-for="todo in todos" />
+
+
+
+  <!-- vue/no-use-v-if-with-v-for -->
+
+  <!-- ✓ GOOD -->
+  <ul v-if="complete">
+    <TodoItem
+      v-for="todo in todos"
+      :key="todo.id"
+    />
+  </ul>
+  <TodoItem
+    v-for="todo in shownTodos"
+    :key="todo.id"
+  />
+
+  <!-- ✗ BAD -->
+  <TodoItem
+    v-for="todo in todos"
+    v-if="complete"
+    :key="todo.id"
+  /><!-- ↑ In this case, the `v-if` should be written on the wrapper element. -->
+  <TodoItem
+    v-for="todo in todos"
+    v-if="todo.shown"
+    :key="todo.id"
+  /><!-- ↑ In this case, the `v-for` list variable should be replaced with a computed property that returns your filtered list. You should not mix 'v-for' with 'v-if' -->
+</template>
+
+<script>
+export default {}
+</script>
+<style lang=""></style>
